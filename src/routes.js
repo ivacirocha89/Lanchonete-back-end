89869e81675ce12b0b35bd47f9e1f0fbbab0a177
ivacirocha89 <ivaci.c.rocha@gmail.com>
2025-01-30 09:17:21 -0300

    this commit was created the Controller Products and created the products Controller routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import UserController from './app/controller/UserController.js'
 import SessionController from './app/controller/SessionController.js'
+import ProductController from './app/controller/ProductController.js'
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
+
+routes.post('/products', ProductController.store)
 
 export default routes
