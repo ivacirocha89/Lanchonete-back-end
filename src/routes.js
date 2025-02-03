070c32e35fcbde527c9356b58fbed2e3b1642588
ivacirocha89 <ivaci.c.rocha@gmail.com>
2025-02-03 08:36:28 -0300

    created file multer.js and folder uploads to add images to projects

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer.js'
+
 import UserController from './app/controller/UserController.js'
 import SessionController from './app/controller/SessionController.js'
 import ProductController from './app/controller/ProductController.js'
+
+const upload = multer(multerConfig)
+
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
 
-routes.post('/products', ProductController.store)
+routes.post('/products', upload.single('file'), ProductController.store)
 
 export default routes
